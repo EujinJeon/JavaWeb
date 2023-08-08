1c80a6708a9e9eabd821b4a13bd49364308326b3
Eujin Jeon <evil0308@gmail.com>
2023-08-08 15:14:31 +0900

    commit 2023-08-08

--- src/main/java/com/example/javaweb/controller/PostsApiController.java
+++ src/main/java/com/example/javaweb/controller/PostsApiController.java
@@ -1,11 +1,10 @@
 package com.example.javaweb.controller;
 
 import com.example.javaweb.controller.dto.PostsSaveRequestDto;
+import com.example.javaweb.controller.dto.PostsUpdateRequestDto;
 import com.example.javaweb.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,5 +15,11 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto)
+    {
+        return postsService.update(id, requestDto);
     }
 }
--- src/main/java/com/example/javaweb/service/posts/PostsService.java
+++ src/main/java/com/example/javaweb/service/posts/PostsService.java
@@ -1,6 +1,9 @@
 package com.example.javaweb.service.posts;
 
+import com.example.javaweb.controller.dto.PostsResponseDto;
 import com.example.javaweb.controller.dto.PostsSaveRequestDto;
+import com.example.javaweb.controller.dto.PostsUpdateRequestDto;
+import com.example.javaweb.domain.posts.Posts;
 import com.example.javaweb.domain.posts.PostsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,5 +18,22 @@ public class PostsService {
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity())
                 .getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() ->
+                        new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id){
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new
+                        IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+        return new PostsResponseDto(entity);
     }
 }
